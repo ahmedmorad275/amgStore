dc9ccc91af23de9a9c8cda1c4583efea2b7f5b7a
ahmedmorad275 <ahmedmoradg30@gmail.com>
2026-01-13 16:47:00 +0200

    Added Loader

--- script.js
+++ script.js
@@ -199,6 +199,24 @@ cartCloseBtn.addEventListener('click', () => {
 
 // Getting smartPhones .........=
 const phoneCards = document.querySelector('.phoneCards');
+function showLoader(ele) {
+  ele.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="loaderContainer">
+              <div class="loader"></div>
+            </div>
+    `
+  );
+}
+
+function hideLoader(ele) {
+  const loader = ele.querySelector('.loaderContainer');
+  if (loader) {
+    loader.remove();
+  }
+}
+
 function renderSmartPhones(data = []) {
   data.forEach((phone) => {
     phoneCards.insertAdjacentHTML(
@@ -236,6 +254,7 @@ function renderSmartPhones(data = []) {
 const loader = document.querySelector('.loaderContainer');
 
 async function getSmartPhones() {
+  showLoader(phoneCards);
   try {
     const response = await fetch(
       'https://dummyjson.com/products/category/smartphones'
@@ -245,16 +264,11 @@ async function getSmartPhones() {
     }
     const data = await response.json();
     const smartPhones = data.products.splice(0, 6);
-    if (!smartPhones) {
-      loader.style.display = 'flex';
-      phoneCards.style.padding = '80px 0 0 0';
-    } else {
-      loader.style.display = 'none';
-      phoneCards.style.padding = '0 0 0 0';
-      renderSmartPhones(smartPhones);
-    }
+    renderSmartPhones(smartPhones);
   } catch (error) {
     console.error(error);
+  } finally {
+    hideLoader(phoneCards);
   }
 }
 
@@ -292,310 +306,47 @@ const toast = document.querySelector('.toast');
 // });
 // Toast Function .........=
 
-// const data = [
-//   {
-//     id: 121,
-//     title: "iPhone 5s",
-//     description:
-//       "The iPhone 5s is a classic smartphone known for its compact design and advanced features during its release. While it's an older model, it still provides a reliable user experience.",
-//     category: "smartphones",
-//     price: 199.99,
-//     discountPercentage: 12.91,
-//     rating: 2.83,
-//     stock: 25,
-//     tags: ["smartphones", "apple"],
-//     brand: "Apple",
-//     sku: "SMA-APP-IPH-121",
-//     weight: 2,
-//     dimensions: {
-//       width: 5.29,
-//       height: 18.38,
-//       depth: 17.72,
-//     },
-//     warrantyInformation: "Lifetime warranty",
-//     shippingInformation: "Ships in 1 month",
-//     availabilityStatus: "In Stock",
-//     reviews: [
-//       {
-//         rating: 5,
-//         comment: "Highly recommended!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Jace Smith",
-//         reviewerEmail: "jace.smith@x.dummyjson.com",
-//       },
-//       {
-//         rating: 1,
-//         comment: "Not as described!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Logan Torres",
-//         reviewerEmail: "logan.torres@x.dummyjson.com",
-//       },
-//       {
-//         rating: 5,
-//         comment: "Very satisfied!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Harper Kelly",
-//         reviewerEmail: "harper.kelly@x.dummyjson.com",
-//       },
-//     ],
-//     returnPolicy: "60 days return policy",
-//     minimumOrderQuantity: 3,
-//     meta: {
-//       createdAt: "2025-04-30T09:41:02.054Z",
-//       updatedAt: "2025-04-30T09:41:02.054Z",
-//       barcode: "8814683940853",
-//       qrCode: "https://cdn.dummyjson.com/public/qr-code.png",
-//     },
-//     images: [
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-5s/1.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-5s/2.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-5s/3.webp",
-//     ],
-//     thumbnail:
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-5s/thumbnail.webp",
-//   },
-//   {
-//     id: 122,
-//     title: "iPhone 6",
-//     description:
-//       "The iPhone 6 is a stylish and capable smartphone with a larger display and improved performance. It introduced new features and design elements, making it a popular choice in its time.",
-//     category: "smartphones",
-//     price: 299.99,
-//     discountPercentage: 6.69,
-//     rating: 3.41,
-//     stock: 60,
-//     tags: ["smartphones", "apple"],
-//     brand: "Apple",
-//     sku: "SMA-APP-IPH-122",
-//     weight: 7,
-//     dimensions: {
-//       width: 11,
-//       height: 9.1,
-//       depth: 9.67,
-//     },
-//     warrantyInformation: "1 month warranty",
-//     shippingInformation: "Ships overnight",
-//     availabilityStatus: "In Stock",
-//     reviews: [
-//       {
-//         rating: 3,
-//         comment: "Disappointing product!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Stella Morris",
-//         reviewerEmail: "stella.morris@x.dummyjson.com",
-//       },
-//       {
-//         rating: 4,
-//         comment: "Excellent quality!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Nolan Gonzalez",
-//         reviewerEmail: "nolan.gonzalez@x.dummyjson.com",
-//       },
-//       {
-//         rating: 5,
-//         comment: "Great value for money!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Benjamin Foster",
-//         reviewerEmail: "benjamin.foster@x.dummyjson.com",
-//       },
-//     ],
-//     returnPolicy: "7 days return policy",
-//     minimumOrderQuantity: 5,
-//     meta: {
-//       createdAt: "2025-04-30T09:41:02.054Z",
-//       updatedAt: "2025-04-30T09:41:02.054Z",
-//       barcode: "9922357685013",
-//       qrCode: "https://cdn.dummyjson.com/public/qr-code.png",
-//     },
-//     images: [
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-6/1.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-6/2.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-6/3.webp",
-//     ],
-//     thumbnail:
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-6/thumbnail.webp",
-//   },
-//   {
-//     id: 123,
-//     title: "iPhone 13 Pro",
-//     description:
-//       "The iPhone 13 Pro is a cutting-edge smartphone with a powerful camera system, high-performance chip, and stunning display. It offers advanced features for users who demand top-notch technology.",
-//     category: "smartphones",
-//     price: 1099.99,
-//     discountPercentage: 9.37,
-//     rating: 4.12,
-//     stock: 56,
-//     tags: ["smartphones", "apple"],
-//     brand: "Apple",
-//     sku: "SMA-APP-IPH-123",
-//     weight: 8,
-//     dimensions: {
-//       width: 12.63,
-//       height: 5.28,
-//       depth: 14.29,
-//     },
-//     warrantyInformation: "3 year warranty",
-//     shippingInformation: "Ships in 2 weeks",
-//     availabilityStatus: "In Stock",
-//     reviews: [
-//       {
-//         rating: 5,
-//         comment: "Would buy again!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Christian Perez",
-//         reviewerEmail: "christian.perez@x.dummyjson.com",
-//       },
-//       {
-//         rating: 3,
-//         comment: "Not worth the price!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Liam Gonzalez",
-//         reviewerEmail: "liam.gonzalez@x.dummyjson.com",
-//       },
-//       {
-//         rating: 5,
-//         comment: "Very satisfied!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Tristan Scott",
-//         reviewerEmail: "tristan.scott@x.dummyjson.com",
-//       },
-//     ],
-//     returnPolicy: "7 days return policy",
-//     minimumOrderQuantity: 1,
-//     meta: {
-//       createdAt: "2025-04-30T09:41:02.054Z",
-//       updatedAt: "2025-04-30T09:41:02.054Z",
-//       barcode: "4998438802308",
-//       qrCode: "https://cdn.dummyjson.com/public/qr-code.png",
-//     },
-//     images: [
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-13-pro/1.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-13-pro/2.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-13-pro/3.webp",
-//     ],
-//     thumbnail:
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-13-pro/thumbnail.webp",
-//   },
-//   {
-//     id: 124,
-//     title: "iPhone X",
-//     description:
-//       "The iPhone X is a flagship smartphone featuring a bezel-less OLED display, facial recognition technology (Face ID), and impressive performance. It represents a milestone in iPhone design and innovation.",
-//     category: "smartphones",
-//     price: 899.99,
-//     discountPercentage: 19.59,
-//     rating: 2.51,
-//     stock: 37,
-//     tags: ["smartphones", "apple"],
-//     brand: "Apple",
-//     sku: "SMA-APP-IPH-124",
-//     weight: 1,
-//     dimensions: {
-//       width: 21.88,
-//       height: 24.19,
-//       depth: 14.19,
-//     },
-//     warrantyInformation: "3 months warranty",
-//     shippingInformation: "Ships in 3-5 business days",
-//     availabilityStatus: "In Stock",
-//     reviews: [
-//       {
-//         rating: 4,
-//         comment: "Highly recommended!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Tyler Davis",
-//         reviewerEmail: "tyler.davis@x.dummyjson.com",
-//       },
-//       {
-//         rating: 5,
-//         comment: "Great product!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Aria Parker",
-//         reviewerEmail: "aria.parker@x.dummyjson.com",
-//       },
-//       {
-//         rating: 2,
-//         comment: "Not as described!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Lily Torres",
-//         reviewerEmail: "lily.torres@x.dummyjson.com",
-//       },
-//     ],
-//     returnPolicy: "7 days return policy",
-//     minimumOrderQuantity: 2,
-//     meta: {
-//       createdAt: "2025-04-30T09:41:02.054Z",
-//       updatedAt: "2025-04-30T09:41:02.054Z",
-//       barcode: "3034949322264",
-//       qrCode: "https://cdn.dummyjson.com/public/qr-code.png",
-//     },
-//     images: [
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-x/1.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-x/2.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-x/3.webp",
-//     ],
-//     thumbnail:
-//       "https://cdn.dummyjson.com/product-images/smartphones/iphone-x/thumbnail.webp",
-//   },
-//   {
-//     id: 125,
-//     title: "Oppo A57",
-//     description:
-//       "The Oppo A57 is a mid-range smartphone known for its sleek design and capable features. It offers a balance of performance and affordability, making it a popular choice.",
-//     category: "smartphones",
-//     price: 249.99,
-//     discountPercentage: 2.43,
-//     rating: 3.94,
-//     stock: 19,
-//     tags: ["smartphones", "oppo"],
-//     brand: "Oppo",
-//     sku: "SMA-OPP-OPP-125",
-//     weight: 5,
-//     dimensions: {
-//       width: 7.2,
-//       height: 10.74,
-//       depth: 23.68,
-//     },
-//     warrantyInformation: "Lifetime warranty",
-//     shippingInformation: "Ships in 3-5 business days",
-//     availabilityStatus: "In Stock",
-//     reviews: [
-//       {
-//         rating: 4,
-//         comment: "Excellent quality!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Scarlett Wright",
-//         reviewerEmail: "scarlett.wright@x.dummyjson.com",
-//       },
-//       {
-//         rating: 5,
-//         comment: "Excellent quality!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Jacob Cooper",
-//         reviewerEmail: "jacob.cooper@x.dummyjson.com",
-//       },
-//       {
-//         rating: 2,
-//         comment: "Poor quality!",
-//         date: "2025-04-30T09:41:02.054Z",
-//         reviewerName: "Zoe Nicholson",
-//         reviewerEmail: "zoe.nicholson@x.dummyjson.com",
-//       },
-//     ],
-//     returnPolicy: "90 days return policy",
-//     minimumOrderQuantity: 3,
-//     meta: {
-//       createdAt: "2025-04-30T09:41:02.054Z",
-//       updatedAt: "2025-04-30T09:41:02.054Z",
-//       barcode: "0651223722522",
-//       qrCode: "https://cdn.dummyjson.com/public/qr-code.png",
-//     },
-//     images: [
-//       "https://cdn.dummyjson.com/product-images/smartphones/oppo-a57/1.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/oppo-a57/2.webp",
-//       "https://cdn.dummyjson.com/product-images/smartphones/oppo-a57/3.webp",
-//     ],
-//     thumbnail:
-//       "https://cdn.dummyjson.com/product-images/smartphones/oppo-a57/thumbnail.webp",
-//   },
-// ];
+// Daily Essentials .........=
+const dailyEssBrandsContainer = document.querySelector(
+  '.dailyEssBrandsContainer'
+);
+function renderDailyEssentials(data = []) {
+  data.forEach((product) => {
+    dailyEssBrandsContainer.insertAdjacentHTML(
+      'beforeend',
+      `
+      <article class="dailyEssBrandContainer">
+              <div class="imageBox">
+                <img src=${product.images[0]} alt="${product.title}" />
+              </div>
+              <div class="dailyEssTextBox">
+                <p class="dailyEssTitle">${product.title}</p>
+                <p class="dailyEssDiscount">Up to ${Math.round(
+                  product.discountPercentage
+                )} OFF</p>
+              </div>
+            </article>
+      `
+    );
+  });
+}
+async function getDailyEssentials() {
+  showLoader(dailyEssBrandsContainer);
+  try {
+    const response = await fetch(
+      'https://dummyjson.com/products/category/groceries'
+    );
+    if (!response.ok) {
+      throw new Error(`Response Status: ${response.status}`);
+    }
+    const data = await response.json();
+    const dailyEss = data.products.splice(0, 8);
+    renderDailyEssentials(dailyEss);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    hideLoader(dailyEssBrandsContainer);
+  }
+}
+getDailyEssentials();
+// Daily Essentials .........=
